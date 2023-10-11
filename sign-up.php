d3e01bfccfe2eaee183851b99c53a8f3f28fe0dc
Aaron Foote <afoote@wesleyan.edu>
2023-10-11 10:20:13 -0400

    Sign Up Page issue-04
    Added the skeleton of the sign up page

---
+++ sign-up.php
@@ -0,0 +1,22 @@
+<html>
+<head></head>
+<body>
+    <h1> Sign Up for StarTunes! </h1>
+    <br /><h2> Fill this out </h2>
+    <p>
+        <label> Username </label>
+        <input type = "text" id = "username">
+    </p>
+    <p>
+        <label> Password </label>
+        <input type = "text" id = "pass">
+    </p>
+    <p>
+        <label> Confirm Password </label>
+        <input type = "text" id = "passConfirm">
+    </p>
+    <p><input type  = "submit" id = "button" value = "Submit"></p>
+    <p><input type  = "reset" id = "button" value = "Reset"></p>
+    <br /><p> Already have an account? <a href = "index.php"> Log in here </a></p>
+</body>
+</html>
