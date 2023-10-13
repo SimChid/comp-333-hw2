8a0ea81e7a81f245a62d1b64bc85c814d1a1c0b7
Aaron Foote <afoote@wesleyan.edu>
2023-10-13 17:45:44 -0400

    Issue 15
    Started working on connecting the sign up page to the ratings page

--- ratingsPage.php
+++ ratingsPage.php
@@ -3,5 +3,11 @@
 <body>
     <h1> Sign Up for StarTunes! </h1>
     <p> More to come... </p>
+    <?php 
+        session_start();
+        echo('PHPSESSID: ' . session_id($_GET['session_id']));
+    ?>
+    
+
 </body>
 </html>
