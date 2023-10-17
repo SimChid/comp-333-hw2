3c4276c07c1bcbfe407543d5564dbc9b398af2fd
Aaron Foote <afoote@wesleyan.edu>
2023-10-16 23:14:01 -0400

    Updating Ratings

--- ratingsPage.php
+++ ratingsPage.php
@@ -16,19 +16,13 @@
     <p><a href = "index.php">Log Out</a></p>
     <h1> StarTunes Catalogue </h1>
     <p><a href = "create.php">Add New Song Rating</a></p>
+    <table>
+        <tbody>
+            <tr>
+            <th>ID</th> <th>Username</th> <th>Artist</th> <th>Song</th><th>Rating</th><th>Action</th></tr>
     <?php
         //Please note that ChatGPT was used to fix the table code because it wasn't working initially
         //https://chat.openai.com/share/12ab7911-c122-4bdd-8e16-9781239dd62f
-        echo "<table>";
-        echo "<tbody>";
-        echo "<tr>";
-        echo "<th>ID</th>";
-        echo "<th>Username</th>";
-        echo "<th>Artist</th>";
-        echo "<th>Song</th>";
-        echo "<th>Rating</th>";
-        echo "<th>Action</th>";
-        echo "</tr>";
 
         // Fetch the maximum ID first
         $maxIdResult = mysqli_query($db, "SELECT MAX(id) AS max_id FROM ratings");
@@ -47,16 +41,15 @@
                 echo "<td>" . $rating['song'] . "</td>";
                 echo "<td>" . $rating['rating'] . "</td>";
                 if ($user == $rating['username']) {
-                    echo "<td><a href='view.php'>View</a> <a href='update.php'>Update</a> <a href='delete.php'>Delete</a></td>";
+                    echo "<td><a href='view.php'>View</a> <a href='update.php?songID=$i'>Update</a> <a href='delete.php'>Delete</a></td>";
                 } else {
                     echo "<td><a href='view.php'>view</a></td>";
                 }
                 echo "</tr>";
             }
         }
-
-        echo "</tbody>";
-        echo "</table>";
     ?>
+    </tbody>
+    </table>
 </body>
 </html>
